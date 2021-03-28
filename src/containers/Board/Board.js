9d6e660c69455a72cc82e9bcb3c8388e692b7c8a
Alapan Chatterjee <alapan.chatterjee@cyware.com>
2021-03-29 01:15:02 +0530

    feat: following changes - 1. Handle default snake generation & food cell generation. 2. Auto movemenet of snake cells.

--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,37 +1,100 @@
 import React, { Component } from "react";
-import { createBoard } from "../../lib/utils";
+import { createBoard, getStartingSnakeLLValue } from "../../lib/utils";
+import LinkedList from "../../lib/SinglyLinkedList";
 
 import classes from "./Board.module.css";
 
 const BOARD_SIZE = 10;
+const Direction = {
+    LEFT: "left",
+    RIGHT: "right",
+    UP: "up",
+    DOWN: "down"
+};
 
 class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
             board: [],
-            snakeCells: null
+            snakeCells: null,
+            foodCell: null,
+            snake: null,
+            direction: Direction.RIGHT
         }
     }
 
     componentDidMount() {
-       this.setState({
-           board: createBoard(BOARD_SIZE),
-           snakeCells: new Set([44])
-       });
+        const board = createBoard(BOARD_SIZE);
+        const snake = new LinkedList().push(getStartingSnakeLLValue(board));
+    
+        this.setState({ 
+           board, 
+           snake,
+           snakeCells: new Set([snake.head.value.cell]), 
+           foodCell: snake.head.value.cell + 5 
+        });
+
+       setInterval(() => {
+           this.moveSnake();
+       }, 1500);
+    }
+
+    moveSnake() {
+        const { snake, snakeCells, direction, board } = this.state;
+        const { value: { row, col } } = snake.head;
+        const currentHeadCoords = { row, col };
+
+        //Get next head co ordinates and values
+        const nextHeadCoords = this.getNextHeadCoords(currentHeadCoords, direction);
+        const nextHeadCell = board[nextHeadCoords.row][nextHeadCoords.col];
+
+        //Remove old head & set new head
+        const oldHead = snake.shift();
+        const newHead = snake.push({ row: nextHeadCoords.row, col: nextHeadCoords.col, cell: nextHeadCell });
+        
+        const newSnakeCells = new Set(snakeCells);
+        newSnakeCells.delete(oldHead.value.cell);
+        newSnakeCells.add(nextHeadCell);
+
+        this.setState({
+            snakeCells: newSnakeCells,
+            snake: newHead
+        });
+    }
+
+    getNextHeadCoords(currentHeadCoords, direction) {
+        let updatedHeadCoords = {};
+        const { row, col } = currentHeadCoords;
+        switch (direction) {
+            case Direction.UP:
+                updatedHeadCoords = { row: row - 1 < 0 ? BOARD_SIZE - 1 : row - 1, col };
+                break;
+            case Direction.DOWN:
+                updatedHeadCoords = { row: row + 1 >= BOARD_SIZE ? 0 : row + 1, col };
+                break;
+            case Direction.LEFT:
+                updatedHeadCoords = { row, col: col - 1 < 0 ? BOARD_SIZE - 1: col - 1 };
+                break;
+            case Direction.RIGHT: 
+                updatedHeadCoords = { row, col: col + 1 >= BOARD_SIZE ? 0 : col + 1 };
+                break;
+            default:
+                break;
+        }
+        return updatedHeadCoords;
     }
 
     render() {
-        const { board, snakeCells } = this.state;
+        const { board, foodCell, snakeCells } = this.state;
         return <div className={classes.Board}>
             {
                 board.map((row, rowIdx) => 
                     <div key={rowIdx} className={classes.Board__row}>
                         {
-                            row.map((counter, cellIdx) => {
-                                const classArr = [classes.Board__cell, snakeCells.has(counter) ? classes['Board__snake-cell'] : ''];
-                                return <div key={cellIdx} className={classArr.join(' ')}>
-                                    { counter }
+                            row.map((cellValue, cellIdx) => {
+                                return <div key={cellIdx} className={getClassNames(cellValue, foodCell, snakeCells)}>
+                                    { cellValue }
                                 </div>;
                             })
                         }
@@ -40,6 +103,17 @@ class Board extends Component {
             }
         </div>
     }
+}
+
+const getClassNames = (cellValue, foodCell, snakeCells) => {
+    let classNames = [classes.Board__cell];
+    if(cellValue === foodCell) {
+        classNames.push(classes['Board__cell--red']); 
+    }
+    if(snakeCells.has(cellValue)) {
+        classNames.push(classes['Board__cell--green']); 
+    }
+    return classNames.join(' ');
 }
 
 export default Board;
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,4 +8,15 @@ function createBoard(BOARD_SIZE) {
     );
 }
 
-export { randomIntFromInterval, createBoard };
+function getStartingSnakeLLValue(board) {
+    const rows = board.length, cols = board[0].length;
+    const startingRow = Math.round(rows / 3), startingCol = Math.round(cols / 3);
+    const startingCellvalue = board[startingRow][startingCol];
+    return { 
+        row: startingRow,
+        col: startingCol,
+        cell: startingCellvalue
+    }
+}
+
+export { randomIntFromInterval, createBoard, getStartingSnakeLLValue };
